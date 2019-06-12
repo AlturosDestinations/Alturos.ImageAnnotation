7ec3b4fa6e3be159de89e88385b93738fec5fd29
Tino Hager <tino.hager@alturos.com>
2019-06-12 13:24:09 +0200

    Increase version

--- src/Alturos.ImageAnnotation/Properties/AssemblyInfo.cs
+++ src/Alturos.ImageAnnotation/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Alturos.ImageAnnotation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alturos Destinations GmbH")]
 [assembly: AssemblyProduct("Alturos.ImageAnnotation")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
