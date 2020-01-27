d4af59601f1a0853ec25c0ac80175a0361f4a6c9
Lukas Kneringer <lukas.kneringer@alturos.com>
2020-01-27 08:54:58 +0100

    Add Split Container to Draw Control

--- src/Alturos.ImageAnnotation/CustomControls/AnnotationDrawControl.Designer.cs
+++ src/Alturos.ImageAnnotation/CustomControls/AnnotationDrawControl.Designer.cs
@@ -36,9 +36,14 @@
             this.contextMenuStripPicture = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.clearAnnotationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.legendsChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
             this.rotatingPictureBox = new Alturos.ImageAnnotation.CustomControls.RotatingPictureBox();
             this.contextMenuStripPicture.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.legendsChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.rotatingPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,9 +64,8 @@
             // 
             // legendsChart
             // 
-            this.legendsChart.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.legendsChart.BackColor = System.Drawing.Color.Transparent;
+            this.legendsChart.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Alignment = System.Drawing.StringAlignment.Center;
             legend1.BackColor = System.Drawing.Color.Transparent;
             legendItem1.Color = System.Drawing.Color.Aqua;
@@ -74,26 +78,46 @@
             legend1.IsDockedInsideChartArea = false;
             legend1.Name = "Legend1";
             this.legendsChart.Legends.Add(legend1);
-            this.legendsChart.Location = new System.Drawing.Point(0, 287);
+            this.legendsChart.Location = new System.Drawing.Point(0, 0);
             this.legendsChart.Name = "legendsChart";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.legendsChart.Series.Add(series1);
-            this.legendsChart.Size = new System.Drawing.Size(624, 49);
+            this.legendsChart.Size = new System.Drawing.Size(624, 42);
             this.legendsChart.TabIndex = 2;
             this.legendsChart.Text = "chart1";
             // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.rotatingPictureBox);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.legendsChart);
+            this.splitContainer.Size = new System.Drawing.Size(624, 336);
+            this.splitContainer.SplitterDistance = 290;
+            this.splitContainer.TabIndex = 4;
+            // 
             // rotatingPictureBox
             // 
+            this.rotatingPictureBox.ContextMenuStrip = this.contextMenuStripPicture;
             this.rotatingPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.rotatingPictureBox.Image = null;
             this.rotatingPictureBox.Location = new System.Drawing.Point(0, 0);
             this.rotatingPictureBox.Name = "rotatingPictureBox";
-            this.rotatingPictureBox.Size = new System.Drawing.Size(624, 336);
+            this.rotatingPictureBox.Size = new System.Drawing.Size(624, 290);
+            this.rotatingPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.rotatingPictureBox.TabIndex = 3;
             this.rotatingPictureBox.TabStop = false;
-            this.rotatingPictureBox.ContextMenuStrip = this.contextMenuStripPicture;
-            this.rotatingPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.rotatingPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
             this.rotatingPictureBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDoubleClick);
             this.rotatingPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
@@ -106,14 +130,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.rotatingPictureBox);
-            this.Controls.Add(this.legendsChart);
-            this.Controls.Add(this.rotatingPictureBox);
+            this.Controls.Add(this.splitContainer);
             this.Name = "AnnotationDrawControl";
             this.Size = new System.Drawing.Size(624, 336);
-            ((System.ComponentModel.ISupportInitialize)(this.rotatingPictureBox)).EndInit();
             this.contextMenuStripPicture.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.legendsChart)).EndInit();
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.rotatingPictureBox)).EndInit();
             this.ResumeLayout(false);
 
@@ -125,5 +150,6 @@
         private System.Windows.Forms.ToolStripMenuItem clearAnnotationsToolStripMenuItem;
         private System.Windows.Forms.DataVisualization.Charting.Chart legendsChart;
         private RotatingPictureBox rotatingPictureBox;
+        private System.Windows.Forms.SplitContainer splitContainer;
     }
 }
